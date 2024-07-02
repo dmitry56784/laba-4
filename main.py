93a21c4c36a97cb9402bdbd8d2363200b288e66d
dmitriypolitov <d.i.politov@students.psu.by>
2024-07-02 09:57:48 +0300

    Add file

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello world")
+import random
+
+
+def generate_password(length):
+    characters = list("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+    password = ""
+    for i in range(length):
+        password += random.choice(characters)
+    return password
+def main():
+    num_passwords = int(input("Введите количество паролей: "))
+    password_length = int(input("Введите длину паролей: "))
+    passwords = []
+    for i in range(num_passwords):
+        passwords.append(generate_password(password_length))
+    for password in passwords:
+        print(password)
+if __name__ == "__main__":
+    main()
